7ac5e41f4ee943f7eba222d49085403b33d9be78
Blue Dragon <andresand066@gmail.com>
2023-10-30 22:21:39 -0500

    Despliegue en Netlify

---
+++ bluedragon.js
@@ -0,0 +1,36 @@
+// let persona = {
+//     nombre : "Ika",
+//     edad : 20 ,
+//     fecha : 4,
+//     Paìs : 'Colombia',
+// }
+
+// if (edad >= 18) {
+
+//     console.log("Eres mayor de edad");
+    
+// } else {
+
+//     console.log('Necesitas la compañía de un adulto');
+    
+// }
+
+alert ('¡Bienvenido a Blue Dragon Tattoo!');
+
+function redireccionCotizacion() {
+	location.href="Cotizacion.html";
+}
+
+function precioCotizacion(params) {
+
+	if (condition) {
+	
+	} else {
+		
+	}
+	
+}
+
+
+
+
